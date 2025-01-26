d412fe70974c1a9cc4eca386de6e04ce154a8e6c
Josef LaFranchise <43555399+jjlnbpt@users.noreply.github.com>
2025-01-26 16:18:38 -0500

    Press esc to exit

---
+++ Assets/ExitGame.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
